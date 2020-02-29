62babf2048dedc23d05e761f1d6576f35cf472ba
Eason963 <Eason963@github.com>
2020-02-29 11:51:24 -0500

    test commit

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -2,7 +2,11 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function ProfileScreen() {
-  return <View style={styles.container}></View>;
+  return <View style={styles.container}>
+    <Text>
+      kofhdgfl;khjpgjkhdfhhjkfg
+    </Text>
+  </View>;
 }
 
 const styles = StyleSheet.create({
